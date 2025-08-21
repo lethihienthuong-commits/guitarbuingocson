18262ff69d3a2932daf79e5597e9d08fd1e70daf
LE THI HIEN THUONG <lethihienthuong@gmail.com>
2025-08-21 16:53:46 +0700

    BÙI NGỌC SƠN 2

--- app/header.tsx
+++ app/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="mb-8 flex items-center justify-between">
       <div>
         <Link href="/" className="font-medium text-black dark:text-white">
-          Julien Nim
+            BÙI NGỌC SƠN
         </Link>
         <TextEffect
           as="p"
